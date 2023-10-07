11722c7f4749d1ac6ddb774d2679b3a81972d9ba
IvanShaptalov <qwr111792@gmail.com>
2023-10-07 03:11:44 -0700

    Initial Commit

---
+++ Right on target/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Right on target
+//
+//  Created by van on 07.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
